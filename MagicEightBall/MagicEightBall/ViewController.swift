d72a3fbf048e84796b4f69b3576c7471896f5416
JuanzelS <zel.staton@gmail.com>
2025-11-20 14:01:16 -0800

    commit

---
+++ MagicEightBall/MagicEightBall/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MagicEightBall
+//
+//  Created by Juanzel Staton on 10/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBAction func shakeButtonTapped(_ sender: Any) {
+        func shakeButtonTappedshakeButtonTappedshakeButtonTapped(_ sender: UIButton) {
+        }
+        print("Shake it like a polaroid picture!")
+    }
+    
+}
+
